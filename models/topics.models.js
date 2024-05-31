82383ef40696d48ef21d1476cbd815d55eba5490
Kainath Butt <kainath_butt@hotmail.com>
2024-05-31 12:09:45 +0100

    error handling complete

--- models/topics.models.js
+++ models/topics.models.js
@@ -30,10 +30,6 @@ exports.selectAllComments = (article_id) => {
     return db
     .query("SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC", [article_id] )
     .then(({rows}) => {
-        console.log(rows)
-        // if (!rows.length) {
-        //     return Promise.reject({ status: 404, msg: "Not Found"})
-        // }
         return rows
         })
     }
