46f5b6bbfd3c1ec9703ffa2956ccc6943691d085
Kainath Butt <kainath_butt@hotmail.com>
2024-06-13 14:04:00 +0100

    connection.js file changes made

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,8 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or PGDATABASE not set');
 }
 
 const config = {};
@@ -15,5 +15,4 @@ if (ENV === 'production') {
   config.max = 2
 }
 
-module.exports = new Pool();
 module.exports = new Pool(config);
